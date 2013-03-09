78a2d7c259fe317f492f481cfd186113e6a6f4f1
pasberth <pasberth@gmail.com>
2013-03-09 10:58:45 +0900

    popular の定義を簡潔にした

--- System/Console/OptMatch/Popular.hs
+++ System/Console/OptMatch/Popular.hs
@@ -4,7 +4,4 @@ import Control.Applicative
 import System.Console.OptMatch
 
 popular :: (Functor m, Monad m) => a -> (a -> OptMatchT m a) -> OptMatchT m a
-popular def m = f def where
-  f a = do
-    r <- m a
-    popular r m <|> return r
+popular a m = do { r <- m a; popular r m <|> return r }
