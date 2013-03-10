7c89127fc2b4537b7837691d728abeaa33a466e6
pasberth <pasberth@gmail.com>
2013-03-10 09:29:04 +0900

    意味のない import や LANGUAGE を取り除いた

--- System/Console/OptMatch/Basic.hs
+++ System/Console/OptMatch/Basic.hs
@@ -1,10 +1,6 @@
-{-# LANGUAGE FlexibleContexts #-}
 module System.Console.OptMatch.Basic where
 
-import Control.Applicative
-import Control.Monad.State
 import System.Console.OptMatch
-import System.Console.OptMatch.Types
 import System.Console.OptMatch.Prim
 
 keyword :: Monad m => String -> OptMatchT m String
